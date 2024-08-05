ce4b92d8401fdc2e6483426e198ef5b64ff7f808
ramya-janakiraman <153798183+ramya-janakiraman@users.noreply.github.com>
2024-08-06 00:08:43 +0530

    Add files via upload

---
+++ Day 9/config/CorsConfig.java
@@ -0,0 +1,19 @@
+// package com.tech.course_mania.config;
+
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.lang.NonNull;
+// import org.springframework.web.servlet.config.annotation.CorsRegistry;
+// import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+// @Configuration
+// @EnableWebMvc
+// public class CorsConfig implements WebMvcConfigurer {
+
+// public void addCorsMappings(@NonNull CorsRegistry registry) {
+// registry.addMapping("/**")
+// .allowedOrigins("http://localhost:3000/")
+// .allowedMethods("GET", "POST", "PUT", "DELETE")
+// .allowedHeaders("*");
+// }
+// }
